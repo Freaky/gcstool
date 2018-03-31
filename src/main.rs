be40bcc1a94ffcc0706eb0a04c51161d8ea60cd2
Thomas Hurst <tom@hur.st>
2018-03-31 18:29:29 +0100

    Hmm, sort of...
    Also vec doesn't work like that. No wonder memory use was twice as high
    as expected...

--- src/main.rs
+++ src/main.rs
@@ -83,18 +83,34 @@ impl<T: io::Write> BitWriter for BitBufWriter<T> {
 	}
 }
 
+impl<T: io::Read> BitBufReader<T> {
+	pub fn new(io: T) -> BitBufReader<T> {
+		BitBufReader {
+			io: io,
+			byte: 0,
+			mask: 1
+		}
+	}
+}
+
+
 impl<T: io::Read> BitReader for BitBufReader<T> {
 	fn read_bit(&mut self) -> io::Result<u8> {
 		// println!(" >> {}", bit);
 
-		if self.mask == 128 {
-			try!(self.io.read_exact(&mut [self.byte]));
-			self.mask = 1;
+		if self.mask == 1 {
+			let mut buf = vec![0u8; 1];
+			try!(self.io.read_exact(&mut buf));
+			self.byte = buf[0];
+			// try!(self.io.read_exact(&mut [self.byte]));
+			// println!("read byte {}", self.byte);
+			self.mask = 128;
 		}
 
 		let bit = if self.mask & self.byte > 0 { 1 } else { 0 };
+		// println!(" << {}", bit);
 
-		self.mask <<= 1;
+		self.mask >>= 1;
 
 		Ok(bit)
 	}
@@ -102,11 +118,14 @@ impl<T: io::Read> BitReader for BitBufReader<T> {
 	fn read_bits_u64(&mut self, nbits: u8) -> io::Result<u64> {
 		assert!(nbits < 64);
 
+		// println!("read_bits({})", nbits);
+
 		let mut bits: u64 = 0;
 
-		for _ in 0..nbits {
-			bits &= try!(self.read_bit()) as u64;
-			bits <<= 1;
+		for i in 0..nbits {
+			let bit = try!(self.read_bit()) as u64;
+			// println!("bit {}: {}", i, bit);
+			bits += (1 << ((nbits - i) - 1)) * bit;
 		}
 
 		Ok(bits)
@@ -155,25 +174,10 @@ impl<T: BitWriter> GCSBuilder<T> {
 			encoder: GolombEncoder::new(out, p),
 			n: n,
 			p: p,
-			values: vec![0; n as usize],
+			values: Vec::with_capacity(n as usize),
 			last: 0
 		}
 	}
-
-/*
-	fn add_sorted(&mut self, data: std::string::String) {
-		let h: u64 = u64::from_str_radix(&data[0..15], 16).unwrap() % (self.n * self.p);
-
-		assert!(self.last <= h);
-
-		let diff = h - self.last;
-		self.last = h;
-
-		if diff > 0 {
-			self.encoder.encode(diff);
-		}
-	}
-*/
 
 	fn add(&mut self, data: std::string::String) {
 		let h = u64::from_str_radix(&data[0..15], 16).unwrap() % (self.n * self.p);
@@ -187,6 +191,7 @@ impl<T: BitWriter> GCSBuilder<T> {
 		let mut diff: u64;
 		let mut last: u64 = 0;
 		for v in &self.values {
+			println!(" >> {}", v);
 			diff = v - last;
 			last = *v;
 
@@ -196,6 +201,55 @@ impl<T: BitWriter> GCSBuilder<T> {
 		}
 
 		self.encoder.finish();
+	}
+}
+
+struct GolombDecoder<T> {
+	reader: T,
+	p: u64,
+	log2p: u8
+}
+
+impl<T: BitReader> GolombDecoder<T> {
+	fn new(reader: T, p: u64) -> GolombDecoder<T> {
+		GolombDecoder::<T> {
+			reader: reader,
+			p: p,
+			log2p: (p as f64).log2().ceil().trunc() as u8
+		}
+	}
+
+	fn next(&mut self) -> io::Result<u64> {
+		let mut v: u64 = 0;
+
+		while try!(self.reader.read_bit()) == 1 {
+			v += self.p;
+		}
+
+		v += try!(self.reader.read_bits_u64(self.log2p));
+		Ok(v)
+	}
+}
+
+struct GCSReader<T> {
+	decoder: GolombDecoder<T>,
+	last: u64
+}
+
+impl<T: BitReader> GCSReader<T> {
+	fn new(reader: T, p: u64) -> GCSReader<T> {
+		GCSReader {
+			decoder: GolombDecoder::new(reader, p),
+			last: 0
+		}
+	}
+
+	fn next(&mut self) -> io::Result<u64> {
+		let mut v = try!(self.decoder.next());
+
+		v = self.last + v;
+		self.last = v;
+		Ok(v)
 	}
 }
 
@@ -219,29 +273,22 @@ fn count_lines<R: BufRead + std::io::Seek>(mut inp: R) -> u64 {
 	inp.seek(SeekFrom::Start(0)).expect("seek error");
 	n
 }
-
 /*
-fn is_sorted_file<R: BufRead + std::io::Seek>(mut inp: R) -> bool {
-	let mut sorted = true;
-	let mut last_num:u64 = 0;
+fn test(out_filename: String) {
+	let test_in = File::open(out_filename).expect("can't open database");
+	let test_inbuf = BufReader::new(test_in);
+	let test_bitreader = BitBufReader::new(test_inbuf);
+	let mut decoder = GCSReader::new(test_bitreader, fp);
 
-	{
-		let lines = inp.by_ref().lines().take(10000).take_while(Result::is_ok).map(Result::unwrap);
-		for line in lines {
-			let num = u64::from_str_radix(&line[0..15], 16).unwrap();
-			if last_num > num {
-				sorted = false;
-				break;
-			}
-			last_num = num;
+	loop {
+		let v = decoder.next();
+		match v {
+			Ok(v) => println!(" << {}", v),
+			_ => break
 		}
 	}
-
-	inp.seek(SeekFrom::Start(0)).expect("seek error");
-	return sorted;
 }
 */
-
 fn main() {
 	let args: Vec<String> = env::args().collect();
 
@@ -251,47 +298,59 @@ fn main() {
 	}
 	let in_filename = &args[1];
 	let out_filename = &args[2];
-
-	let mut infile = File::open(in_filename).expect("can't open input");
-	let outfile = File::create(out_filename).expect("can't open output");
-	let mut buf_in = BufReader::new(infile);
-
-	println!("Counting items in {}", in_filename);
-
-	let n = count_lines(&mut buf_in);
-
-	println!("Counted {} items", n);
-
-	println!("Approx memory use: {} MB.", (n * 8) / (1024 * 1024));
-	if n > 1000 * 1000 {
-		println!("^C now and get a better computer if memory constrained");
-		thread::sleep(time::Duration::from_millis(4000));
-	}
-
-	println!("Building Golomb Compressed Set in {}", out_filename);
-
-	let buf_out = BufWriter::new(outfile);
-
-	buf_in.seek(SeekFrom::Start(0)).expect("seek error");
-
 	let fp = FALSE_POSITIVE_RATE;
 
-	let mut count = 0;
-	let bitwriter = BitBufWriter::new(buf_out);
-	let mut gcs = GCSBuilder::new(bitwriter, n, fp);
-	for line in buf_in.lines() {
-		gcs.add(line.unwrap());
+	{
+		let mut infile = File::open(in_filename).expect("can't open input");
+		let outfile = File::create(out_filename).expect("can't open output");
+		let mut buf_in = BufReader::new(infile);
 
-		count += 1;
-		if count % 10_000_000 == 0 {
-			println!(" >> {} of {}, {:.1}%", count, n, (count as f64 / n as f64) * 100.0);
+		println!("Counting items in {}", in_filename);
+
+		let n = count_lines(&mut buf_in);
+
+		println!("Counted {} items", n);
+
+		println!("Approx memory use: {} MB.", (n * 8) / (1024 * 1024));
+		if n > 1000 * 1000 {
+			println!("^C now and get a better computer if memory constrained");
+			thread::sleep(time::Duration::from_millis(4000));
+		}
+
+		println!("Building Golomb Compressed Set in {}", out_filename);
+
+		let buf_out = BufWriter::new(outfile);
+
+		buf_in.seek(SeekFrom::Start(0)).expect("seek error");
+
+
+		let mut count = 0;
+		let bitwriter = BitBufWriter::new(buf_out);
+		{
+			let mut gcs = GCSBuilder::new(bitwriter, n, fp);
+			for line in buf_in.lines() {
+				gcs.add(line.unwrap());
+
+				count += 1;
+				if count % 10_000_000 == 0 {
+					println!(" >> {} of {}, {:.1}%", count, n, (count as f64 / n as f64) * 100.0);
+				}
+			}
+			gcs.finish();
 		}
 	}
-	gcs.finish();
 
-/*
 	let test_in = File::open(out_filename).expect("can't open database");
 	let test_inbuf = BufReader::new(test_in);
 	let test_bitreader = BitBufReader::new(test_inbuf);
-	*/
+	let mut decoder = GCSReader::new(test_bitreader, fp);
+
+	loop {
+		let v = decoder.next();
+		match v {
+			Ok(v) => println!(" << {}", v),
+			_ => break
+		}
+	}
+	
 }
