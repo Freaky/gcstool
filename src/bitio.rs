a0706a84516d1ebe0197ca579b01d85c4cc8cd7d
Thomas Hurst <tom@hur.st>
2018-04-11 01:06:59 +0100

    BitWriter: Rewrite to operate a byte at a time.
    Takes create time on pwned-passwords-2.0 from 395 seconds to 270 on my
    development machine.

--- src/bitio.rs
+++ src/bitio.rs
@@ -43,32 +43,91 @@ impl BitReader {
     }
 }
 
+const MASKS: [u64; 65] = [
+    0x0,
+    0x1,
+    0x3,
+    0x7,
+    0xf,
+    0x1f,
+    0x3f,
+    0x7f,
+    0xff,
+    0x1ff,
+    0x3ff,
+    0x7ff,
+    0xfff,
+    0x1fff,
+    0x3fff,
+    0x7fff,
+    0xffff,
+    0x1ffff,
+    0x3ffff,
+    0x7ffff,
+    0xfffff,
+    0x1fffff,
+    0x3fffff,
+    0x7fffff,
+    0xffffff,
+    0x1ffffff,
+    0x3ffffff,
+    0x7ffffff,
+    0xfffffff,
+    0x1fffffff,
+    0x3fffffff,
+    0x7fffffff,
+    0xffffffff,
+    0x1ffffffff,
+    0x3ffffffff,
+    0x7ffffffff,
+    0xfffffffff,
+    0x1fffffffff,
+    0x3fffffffff,
+    0x7fffffffff,
+    0xffffffffff,
+    0x1ffffffffff,
+    0x3ffffffffff,
+    0x7ffffffffff,
+    0xfffffffffff,
+    0x1fffffffffff,
+    0x3fffffffffff,
+    0x7fffffffffff,
+    0xffffffffffff,
+    0x1ffffffffffff,
+    0x3ffffffffffff,
+    0x7ffffffffffff,
+    0xfffffffffffff,
+    0x1fffffffffffff,
+    0x3fffffffffffff,
+    0x7fffffffffffff,
+    0xffffffffffffff,
+    0x1ffffffffffffff,
+    0x3ffffffffffffff,
+    0x7ffffffffffffff,
+    0xfffffffffffffff,
+    0x1fffffffffffffff,
+    0x3fffffffffffffff,
+    0x7fffffffffffffff,
+    0xffffffffffffffff,
+];
+
 pub struct BitWriter {
-    byte: u8,
-    mask: u8,
+    buffer: u64,
+    unused: u64,
 }
 
 impl BitWriter {
-    pub fn new() -> BitWriter {
-        BitWriter { byte: 0, mask: 128 }
+    pub fn new() -> Self {
+        Self {
+            buffer: 0,
+            unused: 8,
+        }
     }
 
-    pub fn reset(&mut self) {
-        self.byte = 0;
-        self.mask = 128;
-    }
-
+    #[allow(dead_code)]
     pub fn write_bit<T: io::Write>(&mut self, mut io: T, bit: u8) -> io::Result<()> {
         assert!(bit <= 1);
-
-        self.byte += self.mask * bit;
-
-        if self.mask == 1 {
-            self.flush(&mut io)?;
-        } else {
-            self.mask >>= 1;
-        }
-
+        self.write_bits(&mut io, 1, bit as u64)?;
         Ok(())
     }
 
@@ -78,32 +137,38 @@ impl BitWriter {
         nbits: u8,
         value: u64,
     ) -> io::Result<usize> {
-        let mut mask: u64 = 1 << (nbits - 1);
+        assert!(nbits <= 64);
 
-        for _ in 0..nbits {
-            self.write_bit(&mut io, if (value & mask) > 0 { 1 } else { 0 })?;
+        let mut nbits_remaining = nbits as u64;
+        let mut value = value & MASKS[nbits as usize];
 
-            mask >>= 1
+        while nbits_remaining >= self.unused {
+            self.buffer = (self.buffer << self.unused) | (value >> (nbits_remaining - self.unused));
+
+            // write low byte
+            io.write_all(&[self.buffer as u8])?;
+
+            nbits_remaining -= self.unused;
+            value &= MASKS[nbits_remaining as usize];
+            self.unused = 8;
+            self.buffer = 0;
         }
 
+        if nbits_remaining > 0 {
+            self.buffer = (self.buffer << nbits_remaining) | value;
+            self.unused -= nbits_remaining;
+        }
         Ok(nbits as usize)
     }
 
-    // Return the number of padding bits
     pub fn flush<T: io::Write>(&mut self, mut io: T) -> io::Result<usize> {
-        let bits_written: usize = match self.mask {
-            128 => return Ok(0),
-            64 => 7,
-            32 => 6,
-            16 => 5,
-            8 => 4,
-            4 => 3,
-            2 => 2,
-            1 => 1,
-            _ => panic!("invalid mask"),
-        };
-        io.write_all(&[self.byte])?;
-        self.reset();
-        Ok(bits_written)
+        if self.unused != 8 {
+            io.write_all(&[(self.buffer << self.unused) as u8])?;
+            let written = self.unused;
+            self.unused = 8;
+            Ok(written as usize)
+        } else {
+            Ok(0)
+        }
     }
 }
